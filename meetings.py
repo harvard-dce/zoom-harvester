0322a50b224e1f4906efea6d30d6d82272affead
Mona Matthews <monamatthews@dhcp-125-109-217.disted.harvard.edu>
2017-09-26 15:18:46 -0400

    Rewrote harvester to capture meeting topic and host ids in meeting documents. Also now captures sessions (participant sessions) and stores in separate sessions documents.

--- meetings.py
+++ meetings.py
@@ -16,7 +16,6 @@ load_dotenv(join(dirname(__file__), '.env'))
 KEY = getenv('ZOOM_KEY')
 SECRET = getenv('ZOOM_SECRET')
 API_BASE_URL = "https://api.zoom.us/v1"
-MEETING_TYPES = {"live": 1, "past": 2}
 ES_HOST = getenv('ES_HOST')
 
 
@@ -24,76 +23,201 @@ class ZoomApiException(Exception):
     pass
 
 
-def get_meetings_from(date, type, key, secret):
+def fetch_records(report_url, params, wait=1):
+    print("get", report_url, "\nparams", params)
 
-    es = Elasticsearch([ES_HOST])
+    pages = []
 
-    params = {
-        'from': date,
-        'to': date,
-        'type': MEETING_TYPES[type],
-        'api_key': key,
-        'api_secret': secret,
-        'page_size': 100,
-        'page_number': 1
-    }
-
-    meetings = []
     while True:
 
-        r = requests.post(url=API_BASE_URL + "/metrics/meetings", data=params)
+        r = requests.post(url=API_BASE_URL + report_url, data=params)
         r.raise_for_status()
         response = r.json()
 
         if 'error' in response.keys():
             raise ZoomApiException(response['error'])
 
-        for meeting in response['meetings']:
-            document = create_meeting_document(meeting, response['type'])
-            meetings.append(document)
-
-            es.index(
-                index="meetings",
-                doc_type="meeting",
-                id=meeting['uuid'],
-                body=document
-            )
-
-        print("total", type, "meetings:", response['total_records'])
+        pages.append(response)
 
         remaining_pages = response['page_count'] - response['page_number']
 
         if remaining_pages > 0:
-            print("wait", remaining_pages, "more minute(s)")
             params['page_number'] += 1
-            # only 1 zoom api metrics request allowed per minute
-            time.sleep(60)
+            time.sleep(wait)
         else:
             break
 
-    return meetings
+    return pages
 
 
-def create_meeting_document(meeting, type):
+def get_active_users(date):
+    params = {
+        'from': date,
+        'to': date,
+        'api_key': KEY,
+        'api_secret': SECRET,
+        'page_size': 300,  # max page size
+        'page_number': 1
+    }
+
+    pages = fetch_records("/report/getaccountreport", params)
+
+    active_user_ids = []
+
+    for page in pages:
+        for user in page['users']:
+            active_user_ids.append(user['user_id'])
+
+    return active_user_ids
+
+
+def get_series_info(host_ids):
+    series_info = {}  # series ids mapped to topic and host id
+
+    params = {
+        'api_key': KEY,
+        'api_secret': SECRET,
+        'page_size': 300,  # max page size
+        'page_number': 1
+    }
+
+    for host_id in host_ids:
+
+        params['host_id'] = host_id
+
+        pages = fetch_records("/meeting/list", params)
+
+        for page in pages:
+            for meeting in page['meetings']:
+                key = meeting['id']
+                series_info[key] = {'host_id': meeting['host_id'],
+                                    'topic': meeting['topic']}
+
+        time.sleep(1)
+
+    return series_info
+
+
+def get_meeting_uuids(date, key, secret):
+    params = {
+        'from': date,
+        'to': date,
+        'type': 2,  # completed meetings
+        'api_key': key,
+        'api_secret': secret,
+        'page_size': 100,  # max page size
+        'page_number': 1
+    }
+
+    pages = fetch_records("/metrics/meetings", params, wait=60)  # 1 min rate limit
+
+    meeting_uuids = []
+
+    for page in pages:
+        for meeting in page['meetings']:
+            meeting_uuids.append(meeting['uuid'])
+
+    return meeting_uuids
+
+
+def get_meetings_from(date, key, secret):
+    es = Elasticsearch([ES_HOST])
+    url = "/metrics/meetingdetail"
+    meetings = []
+    participant_sessions = []
+
+    params = {
+        'api_key': key,
+        'api_secret': secret,
+        'type': 2,  # completed meetings
+        'page_size': 100,  # max page size
+        'page_number': 1
+    }
+
+    host_ids = get_active_users(date)
+    series_info = get_series_info(host_ids)
+    meeting_uuids = get_meeting_uuids(date, key, secret)
+
+    for uuid in meeting_uuids:
+        params['meeting_id'] = uuid
+        pages = fetch_records(url, params)
+        topic = ""
+        host_id = ""
+
+        series_id = pages[0]['id']
+        if series_id in series_info.keys():
+            topic = series_info[series_id]["topic"]
+            host_id = series_info[series_id]['host_id']
+
+        document = create_meeting_document(pages[0], topic, host_id)
+        meetings.append(document)
+        es.index(
+            index="meetings",
+            doc_type="meeting",
+            id=uuid,  # unique meeting occurrence id
+            body=document
+        )
+
+        for page in pages:
+            for session in page['participants']:
+                document = create_sessions_document(session, uuid)
+                participant_sessions.append(document)
+                # no unique index for participants, can we let elasticsearch autogenerate one?
+                es.index(
+                    index="sessions",
+                    doc_type="session",
+                    body=document
+                )
+
+        time.sleep(1)
+    
+    return meetings, participant_sessions
+
+
+def create_meeting_document(meeting, topic, host_id):
 
     doc = {
-        "reoccurring_id": meeting['id'],
-        "type": type,  # live or past meetings
-        "start_time": meeting['start_time'],
-        "end_time": meeting['end_time'],
-        "duration": to_seconds(meeting['duration']),
+        "meeting_series_id": meeting['id'],
+        "topic": topic,
         "host": {
+            "host_id": host_id,
             "name": meeting['host'],
             "email": meeting['email'],
             "user_type": meeting['user_type']
         },
-        "participant_sessions": meeting['participants'],  # not unique participants
+        "start_time": meeting['start_time'],
+        "end_time": meeting['end_time'],
+        "duration": to_seconds(meeting['duration']),
+        "participant_sessions": len(meeting['participants']),  # not unique participants
         "has_pstn": meeting['has_pstn'],
         "has_voip": meeting['has_voip'],
         "has_3rd_party_audio": meeting['has_3rd_party_audio'],
         "has_video": meeting['has_video'],
         "has_screen_share": meeting['has_screen_share'],
         "recording": meeting['recording'],
+    }
+
+    return doc
+
+
+def create_sessions_document(session, meeting_uuid):
+
+    doc = {
+        "meeting": meeting_uuid,
+        "id": session['id'],
+        "user_id": session['user_id'],
+        "user_name": session['user_name'],
+        "device": session['device'],
+        "ip_address": session['ip_address'],
+        "country": session['cn'],
+        "city": session['city'],
+        "network_type": session['network_type'],
+        "join_time": session['join_time'],
+        "leave_time": session['leave_time'],
+        "share_application": session['share_application'],  # bool
+        "share_desktop": session['share_desktop'],  # bool
+        "share_whiteboard": session['share_whiteboard'],  # bool
+        "recording": session['recording']  # bool
     }
 
     return doc
@@ -114,21 +238,20 @@ def open_destination(destination=None):
 
 # convert duration from MM:SS or HH:MM:SS to seconds
 def to_seconds(duration):
-    duration = duration.split(':')
+    try:
+        dt = datetime.strptime(duration, "%H:%M:%S")
+    except ValueError:
+        dt = datetime.strptime(duration, "%M:%S")
+    delta = timedelta(hours=dt.hour, minutes=dt.minute, seconds=dt.second)
 
-    seconds = int(duration.pop(0)) * 60
-    seconds += int(duration.pop(0))
-    if len(duration) > 0:
-        seconds = (seconds * 60) + int(duration.pop(0))
-
-    return seconds
+    return int(delta.total_seconds())
 
 
 def main(args):
 
     try:
         with open_destination(args.destination) as fh:
-            meetings = get_meetings_from(args.date, args.meeting_type, args.key, args.secret)
+            meetings = get_meetings_from(args.date, args.key, args.secret)
             json.dump(meetings, fh, indent=2)
     except OSError as e:
         print("Destination error: %s" % str(e))
@@ -139,6 +262,7 @@ def main(args):
     except KeyboardInterrupt:
         print("Quitting")
 
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -146,7 +270,6 @@ if __name__ == '__main__':
     parser.add_argument("--secret", help="zoom api secret", default=SECRET)
     parser.add_argument("--date", help="fetch meetings from this date")
     parser.add_argument("--destination", help="destination filename", default="-")
-    parser.add_argument("--meeting-type", help="get live or past meetings", default="past")
     parser.add_argument("--es_host", help="Elasticsearch host:port", default=ES_HOST)
     args = parser.parse_args()
 
